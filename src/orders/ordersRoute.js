5acd5951456b5dd28c883b22abc0d03c3dd3035b
Shotech <40011872+Shotech-E@users.noreply.github.com>
2024-11-16 01:21:03 +0100

    Update ordersRoute.js

--- src/orders/ordersRoute.js
+++ src/orders/ordersRoute.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const Order = require("./ordersModel");
 const verifyToken = require("../middleware/verifyToken");
 const verifyAdmin = require("../middleware/verifyAdmin");
-// const stripe = require("stripe")(
-//   "sk_test_51QE7lZLe2VbPgG0NBFeRqJ6noTxWLyLLWhYkzUvhjuoFcCwWrKPO8bv93T3UFvbh10cwMK2Ai9t2m6Dm0RXBh4zF00f1JAEL1P"
-// );
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // CREATE CHECKOUT SESSION
@@ -27,7 +24,7 @@ router.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `https://showars-frontend-ssyw.vercel.app//success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `https://showars-frontend-ssyw.vercel.app/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `https://showars-frontend-ssyw.vercel.app/cancel`,
     });
     res.json({ id: session.id });
